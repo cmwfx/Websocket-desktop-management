2ade80395f250ee505b29eaed3300b3e64267a2a
William <atozniche1@gmail.com>
2025-03-11 00:33:30 +0200

    Add root index.js and update package.json and Procfile for Heroku

---
+++ index.js
@@ -0,0 +1,5 @@
+// This file is just to help Heroku detect that this is a Node.js application
+// The actual server code is in the server directory
+
+console.log("Starting application...");
+require("./server/index.js");
